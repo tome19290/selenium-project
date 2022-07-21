6ff65447fba3433cc586f9d43567a80e3d9b35fe
saurabh-coding-dojo <108808777+saurabh-coding-dojo@users.noreply.github.com>
2022-07-21 11:31:27 +0530

    Update install_driver.sh

--- install_driver.sh
+++ install_driver.sh
@@ -1,11 +1,3 @@
-wget -N https://chromedriver.storage.googleapis.com/100.0.4896.20/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
-
-
 # Install chrome broswer
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
