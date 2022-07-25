e86289bf649c5c91381ff0bbabacbae421d51e42
Saurabh <saurabh.d2106@gmail.com>
2022-07-25 15:24:16 +0300

    added install.sh

---
+++ install_driver.sh
@@ -0,0 +1,6 @@
+# Install chrome broswer
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
